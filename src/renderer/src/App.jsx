5cacc688b704e706786bbfa466ec3888ee2f9a81
prajays <prajays.work@gmail.com>
2026-04-09 18:43:28 +0530

    version-1

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,33 +1,15 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
-
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <header className="top-bar">
+        <h1>DigitalPet</h1>
+      </header>
+      <main className="main-content">
+        <section className="column">Goals</section>
+        <section className="column">Pomodoro</section>
+        <section className="column">Pet</section>
+      </main>
+    </div>
   )
 }
 
